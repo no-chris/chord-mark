b3cc94e8696bb6f3c92ed9493cc8230277e8b9d4
Christophe Noël <touffi51@gmail.com>
2021-09-14 22:25:15 +0200

    Trim lyric line when chords are not aligned

--- src/renderer/components/renderLyricLine.js
+++ src/renderer/components/renderLyricLine.js
@@ -2,8 +2,12 @@ import lyricLineTpl from './tpl/lyricLine.hbs';
 
 /**
  * @param {SongLyricLine} lyricLine
+ * @param {Boolean} alignChordsWithLyrics
  * @returns {String} rendered html
  */
-export default function render(lyricLine) {
-	return lyricLineTpl({ lyricLine: lyricLine.model.lyrics });
+export default function render(lyricLine, alignChordsWithLyrics = false) {
+	const trimmedLyricLine = alignChordsWithLyrics
+		? lyricLine.model.lyrics
+		: lyricLine.model.lyrics.trim();
+	return lyricLineTpl({ lyricLine: trimmedLyricLine });
 }
