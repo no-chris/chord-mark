2ea8c6141c166f1a156fa97f9ba6c98ff88c9933
Christophe Noël <touffi51@gmail.com>
2021-11-30 22:45:10 +0100

    Add copyright

--- packages/documentation/docusaurus.config.js
+++ packages/documentation/docusaurus.config.js
@@ -101,7 +101,7 @@ const config = {
 						],
 					},
 				],
-				copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+				copyright: `Copyright © ${new Date().getFullYear()} Christophe Noël. Built with Docusaurus.`,
 			},
 			prism: {
 				theme: lightCodeTheme,
