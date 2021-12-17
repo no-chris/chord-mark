07cecaa8b46cae91d7d84782d01d691f836df35d
Christophe Noël <touffi51@gmail.com>
2021-12-17 07:05:48 +0100

    Improve toText() helper with a regepx instead of the .replace() hack

--- packages/chord-mark/tests/unit/helpers/toText.js
+++ packages/chord-mark/tests/unit/helpers/toText.js
@@ -1,11 +1,11 @@
 import stripTags from '../../../src/core/dom/stripTags';
 
 const toText = (chordMark) => {
-	return chordMark
-		.split('</p>')
+	const allLines = chordMark.match(/(<p.*?>.*?<\/p>)/gm);
+
+	return allLines
 		.map((line) => stripTags(line))
 		.map((line) => (line === '&nbsp;' ? '' : line))
-		.slice(0, -1) // the <p> to line conversion adds an extra "\n" that we remove here
 		.join('\n');
 };
 export default toText;
