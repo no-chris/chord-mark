486bb3a64cdda11f4089520ce59ee84272abd6d6
Christophe Noël <touffi51@gmail.com>
2021-12-22 22:42:39 +0100

    Correctly parse song structure and sections

--- packages/chord-mark-converters/src/converters/chordPro2ChordMark.js
+++ packages/chord-mark-converters/src/converters/chordPro2ChordMark.js
@@ -1,5 +1,228 @@
+const lineTypes = {
+	EMPTY: 'empty',
+	DIRECTIVE: 'directive',
+	COMMENT: 'comment',
+	CHORDS: 'chords',
+	LYRICS: 'lyrics',
+	CHORDS_LYRICS: 'chordsLyrics',
+	SECTION_START: 'sectionStart',
+	SECTION_CLOSE: 'sectionClose',
+	SECTION_REPEAT: 'sectionRepeat',
+};
+
+const repeatSectionDirectives = [
+	'chorus',
+	'verse',
+	'bridge',
+	// {Comment} seems to be mostly used to declare repeated sections
+	'comment',
+	'comment_italic',
+	'comment_bold',
+	'c',
+	'ci',
+	'cb',
+];
+
+const startSectionDirectives = [
+	'start_of_chorus',
+	'start_of_verse',
+	'start_of_bridge',
+	'start_of_part',
+	'soc',
+	'sov',
+	'sob',
+	'sop',
+];
+
+const endSectionDirectives = [
+	'end_of_chorus',
+	'end_of_verse',
+	'end_of_bridge',
+	'end_of_part',
+	'eoc',
+	'eov',
+	'eob',
+	'sop',
+];
+
+const directivesToSectionLabel = {
+	chorus: 'c',
+	verse: 'v',
+	bridge: 'b',
+	start_of_chorus: 'c',
+	start_of_verse: 'v',
+	start_of_bridge: 'b',
+	start_of_part: 'v',
+	soc: 'c',
+	sov: 'v',
+	sob: 'b',
+	sop: 'v',
+	// if no value, consider {comment} as a chorus mention ¯\_(ツ)_/¯
+	c: 'c',
+	ci: 'c',
+	cb: 'c',
+	comment: 'c',
+	comment_italic: 'c',
+	comment_bold: 'c',
+};
+
 const chordPro2ChordMark = (chordProInput) => {
-	return chordProInput;
+	const cmOutput = [];
+
+	const srcLines = chordProInput
+		.split('\n')
+		.map((line) => getLineModel(line))
+		.filter((lineModel) => isSupportedLine(lineModel));
+
+	const allSections = getAllSections(srcLines);
+
+	allSections.forEach((section, i) => {
+		cmOutput.push('#' + section.label);
+		section.allLines.forEach((line) => {
+			cmOutput.push(line.content);
+		});
+		if (i < allSections.length - 1) {
+			cmOutput.push('');
+		}
+	});
+
+	return cmOutput.join('\n');
+};
+
+const getLineModel = (line) => {
+	const lineModel = { content: line };
+
+	if (isCommentLine(line)) {
+		lineModel.type = lineTypes.COMMENT;
+	} else if (isEmtpyLine(line)) {
+		lineModel.type = lineTypes.EMPTY;
+	} else if (isDirective(line)) {
+		const [key, value] = getDirectiveKV(line);
+		lineModel.key = key;
+		lineModel.value = value;
+
+		if (startSectionDirectives.includes(key)) {
+			lineModel.type = lineTypes.SECTION_START;
+		} else if (endSectionDirectives.includes(key)) {
+			lineModel.type = lineTypes.SECTION_CLOSE;
+		} else if (repeatSectionDirectives.includes(key)) {
+			lineModel.type = lineTypes.SECTION_REPEAT;
+		} else {
+			lineModel.type = lineTypes.DIRECTIVE;
+		}
+	} else {
+		lineModel.type = lineTypes.CHORDS_LYRICS;
+	}
+
+	return lineModel;
+};
+
+const isCommentLine = (line) => {
+	const commentLineRe = /^#.*$/;
+	const found = line.trim().match(commentLineRe);
+	return found !== null;
+};
+
+const isEmtpyLine = (line) => {
+	return line.trim() === '';
+};
+
+const directiveRe = /^{([^:]+):?(.*)?}$/;
+
+const isDirective = (line) => {
+	const found = line.trim().match(directiveRe);
+	return found !== null;
+};
+
+const getDirectiveKV = (directive) => {
+	const found = directive.trim().match(directiveRe);
+	const key = found[1].trim();
+	const value = found[2] ? found[2].trim() : undefined;
+	return [key, value];
+};
+
+const isSupportedLine = (lineModel) => {
+	const unSupportedLineTypes = [lineTypes.COMMENT, lineTypes.DIRECTIVE];
+	return !unSupportedLineTypes.includes(lineModel.type);
+};
+
+const getAllSections = (allLines) => {
+	let allSections = [];
+	let currentSection;
+
+	const commitCurrentSection = () => {
+		if (currentSection) {
+			allSections.push(currentSection);
+			currentSection = undefined;
+		}
+	};
+
+	const initCurrentSection = (line) => {
+		currentSection = {
+			label: line ? getSectionLabel(line) : 'v',
+			allLines: [],
+		};
+	};
+
+	allLines.forEach((line) => {
+		if (line.type === lineTypes.SECTION_START) {
+			commitCurrentSection();
+			initCurrentSection(line);
+		} else if (line.type === lineTypes.SECTION_CLOSE) {
+			commitCurrentSection();
+		} else if (line.type === lineTypes.SECTION_REPEAT) {
+			commitCurrentSection();
+			initCurrentSection(line);
+			commitCurrentSection();
+		} else if (line.type === lineTypes.EMPTY) {
+			if (currentSection) {
+				currentSection.allLines.push(line);
+			}
+		} else {
+			if (!currentSection) {
+				initCurrentSection();
+			}
+			currentSection.allLines.push(line);
+		}
+	});
+
+	if (currentSection) {
+		allSections.push(currentSection);
+	}
+
+	return allSections.map(trimSection);
+};
+
+const getSectionLabel = (lineModel) => {
+	const labelToShortLabel = {
+		chorus: 'c',
+		verse: 'v',
+		bridge: 'b',
+	};
+
+	let label;
+
+	if (lineModel.value) {
+		label =
+			directivesToSectionLabel[lineModel.value.toLowerCase()] ||
+			lineModel.value;
+	} else {
+		label = directivesToSectionLabel[lineModel.key];
+	}
+	return labelToShortLabel[label] || label;
+};
+
+const trimSection = (section) => {
+	while (section.allLines.length && section.allLines[0].content === '') {
+		section.allLines.shift();
+	}
+	while (
+		section.allLines.length &&
+		section.allLines[section.allLines.length - 1].content === ''
+	) {
+		section.allLines.pop();
+	}
+	return section;
 };
 
 export default chordPro2ChordMark;
--- packages/chord-mark-converters/tests/converters/chordPro2ChordMark/chordPro2ChordMark.spec.js
+++ packages/chord-mark-converters/tests/converters/chordPro2ChordMark/chordPro2ChordMark.spec.js
@@ -14,7 +14,7 @@ describe('Module', () => {
 });
 
 describe('chordPro2ChordMark', () => {
-	describe.each([['base test', input1, output1]])(
+	describe.each([['Sample song 1', input1, output1]])(
 		'%s',
 		(title, input, output) => {
 			test('should produce expected ChordMark markup', () => {
@@ -23,4 +23,174 @@ describe('chordPro2ChordMark', () => {
 			});
 		}
 	);
+
+	describe.each([
+		// ===== Environment directives =====
+		[
+			'recognize verse',
+			`{sov}
+Verse content
+{eov}`,
+			`#v
+Verse content`,
+		],
+		[
+			'recognize chorus',
+			`{soc}
+Chorus content
+{eoc}`,
+			`#c
+Chorus content`,
+		],
+		[
+			'recognize bridge',
+			`{sob}
+Bridge content
+{eob}`,
+			`#b
+Bridge content`,
+		],
+		[
+			'recognize part as verse',
+			`{sop}
+Part content
+{eop}`,
+			`#v
+Part content`,
+		],
+		[
+			'lines without environment should be considered as verse',
+			`Verse content without environment
+
+{soc}
+Chorus content
+{eoc}
+
+Another lonely verse`,
+			`#v
+Verse content without environment
+
+#c
+Chorus content
+
+#v
+Another lonely verse`,
+		],
+
+		// ===== Use label of environment directives =====
+		[
+			'use #c if label is "Chorus"',
+			`{sop: Chorus}\nPart content\n{eop}`,
+			`#c\nPart content`,
+		],
+		[
+			'use #v if label is "Verse"',
+			`{sop: Verse}\nPart content\n{eop}`,
+			`#v\nPart content`,
+		],
+		[
+			'use #b if label is "Bridge"',
+			`{sop: Bridge}\nPart content\n{eop}`,
+			`#b\nPart content`,
+		],
+		[
+			'name section after label',
+			`{sov: myVerse}\nVerse\n{eov}`,
+			`#myVerse\nVerse`,
+		],
+		[
+			'Label might generate an invalid section name',
+			`{sov: My invalid sectionLabel}\nVerse\n{eov}`,
+			`#My invalid sectionLabel\nVerse`,
+		],
+
+		// ===== Section repeat directives =====
+		['chorus repeats', `{chorus}`, `#c`],
+		['verse repeat', `{verse}`, `#v`],
+		['bridge repeats', `{bridge}`, `#b`],
+		['two chorus repeats', `{chorus}\n{chorus}`, `#c\n\n#c`],
+		[
+			'mixed repeats',
+			`{chorus}\n{chorus}\n{verse}\n{bridge}`,
+			`#c\n\n#c\n\n#v\n\n#b`,
+		],
+		['comment with label', `{comment:mySection}`, `#mySection`],
+		['comment without label is considered as chorus', `{comment}`, `#c`],
+		[
+			'comment with long label yields invalid section label',
+			`{comment: My Long Comment}`,
+			`#My Long Comment`,
+		],
+
+		// ===== Directives parsing =====
+		['base', `{comment:mySection}`, `#mySection`],
+		['should tim key and value', `{ comment : mySection   }`, `#mySection`],
+		['one character key/value', `{c:x}`, `#x`],
+		[
+			'should remove unsupported directives',
+			`{start_of_abc}
+{toccolour}
+{define}
+{x_custom_name}
+{new_physical_page}
+{whatever}`,
+			``,
+		],
+
+		// ===== Comment lines =====
+		['should remove comment', `# to-be-removed comment`, ``],
+		[
+			'should remove comment, even if no space after #',
+			`#to-be-removed comment`,
+			``,
+		],
+
+		// ===== Empty lines =====
+		[
+			'should keep empty lines inside sections',
+			`A first line
+
+
+A second line`,
+			`#v
+A first line
+
+
+A second line`,
+		],
+		[
+			'should keep only 1 line between sections',
+			`Verse
+
+
+
+
+{soc}
+Chorus
+{eoc}`,
+			`#v
+Verse
+
+#c
+Chorus`,
+		],
+		[
+			'should trim start and end empty lines',
+			`
+
+{soc}
+Chorus
+{eoc}
+
+
+`,
+			`#c
+Chorus`,
+		],
+	])('%s', (title, input, output) => {
+		test('should produce expected ChordMark markup', () => {
+			const converted = chordPro2ChordMark(input);
+			expect(converted).toBe(output);
+		});
+	});
 });
