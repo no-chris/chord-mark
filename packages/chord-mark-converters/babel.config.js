72d26defc40ef751aab4ae2699ea0660e8f64a94
Christophe Noël <touffi51@gmail.com>
2022-01-06 23:38:34 +0100

    bundle converters and remove core-js

--- packages/chord-mark-converters/babel.config.js
+++ packages/chord-mark-converters/babel.config.js
@@ -7,11 +7,8 @@ const presets = [
 			targets: {
 				browsers: 'defaults',
 			},
-			useBuiltIns: 'usage',
-			corejs: '3',
 		},
 	],
-	['@babel/preset-react'],
 ];
 
 module.exports = { presets };
