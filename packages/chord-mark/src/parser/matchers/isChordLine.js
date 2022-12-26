7ef724e726930bc77a177a598f748acca5cb84ae
Christophe Noël <touffi51@gmail.com>
2022-12-26 09:16:28 +0100

    detect chord line with sub-beat enclosure symbols

--- packages/chord-mark/src/parser/matchers/isChordLine.js
+++ packages/chord-mark/src/parser/matchers/isChordLine.js
@@ -1,3 +1,4 @@
+import _escapeRegExp from 'lodash/escapeRegExp';
 import clearSpaces from '../helper/clearSpaces';
 
 import syntax from '../syntax';
@@ -7,23 +8,61 @@ const chordBeatCountSymbols = new RegExp(syntax.chordBeatCount + '*$', 'g');
 const barRepeatSymbols = new RegExp('^' + syntax.barRepeat + '+$');
 
 const getParseableChordLine = (chordLine) => {
-	return chordLine.replace('add #', 'add#').replace('add b', 'addb');
+	return chordLine.replaceAll('add ', 'add');
 };
 
 export default function isChordLine(line = '') {
+	if (hasUnmatchedEnclosureSymbols(line)) return false;
+
 	return clearSpaces(getParseableChordLine(line))
 		.split(' ')
 		.every((potentialChordToken, index) => {
-			const withoutBeatCount = potentialChordToken.replace(
-				chordBeatCountSymbols,
-				''
-			);
+			const clean = cleanToken(potentialChordToken);
+
 			return (
-				isChord(withoutBeatCount) ||
+				isChord(clean) ||
 				(potentialChordToken.match(barRepeatSymbols) && index > 0) ||
-				withoutBeatCount === syntax.noChord
+				clean === syntax.noChord
 			);
 		});
 }
+
+const cleanToken = (token) => {
+	return removeSubBeatEnclosure(removeBeatCount(token));
+};
+
+const removeBeatCount = (token) => {
+	return token.replace(chordBeatCountSymbols, '');
+};
+
+const removeSubBeatEnclosure = (token) => {
+	let clean = token;
+	if (token.startsWith(syntax.subBeatOpener)) {
+		clean = clean.replace(syntax.subBeatOpener, '');
+	}
+	if (token.endsWith(syntax.subBeatCloser) && hasExtraCloseSymbol(token)) {
+		clean = clean.substring(0, clean.length - syntax.subBeatCloser.length);
+	}
+	return clean;
+};
+
+const hasExtraCloseSymbol = (token) => {
+	return (
+		countOccurrences(token, syntax.subBeatCloser) >
+		countOccurrences(token, syntax.subBeatOpener)
+	);
+};
+
+const hasUnmatchedEnclosureSymbols = (token) => {
+	return (
+		countOccurrences(token, syntax.subBeatCloser) !==
+		countOccurrences(token, syntax.subBeatOpener)
+	);
+};
+
+const countOccurrences = (token, target) => {
+	const regex = new RegExp(_escapeRegExp(target), 'g');
+	return (token.match(regex) || []).length;
+};
 
 export { getParseableChordLine };
--- packages/chord-mark/tests/unit/parser/matchers/isChordLine.spec.js
+++ packages/chord-mark/tests/unit/parser/matchers/isChordLine.spec.js
@@ -24,6 +24,13 @@ describe.each([
 	['C(add #9) A(add b9)', true], // remove space in symbol
 	['C(add #9)... A(add b9).', true], // remove space in symbol
 
+	// sub-beats durations
+	['F. (C/E Dm7) C..', true],
+	['(C/E Dm7) F. C..', true],
+	['F. (C/E A(add b9)) C..', true],
+	['(C/E A(add b9) Dm7) F. C..', true],
+	['(A(add b9) C/E Dm7) F. C..', true],
+
 	[undefined, false],
 	['', false],
 	['AB ', false],
@@ -37,6 +44,12 @@ describe.each([
 	['%..', false],
 	['A B %.', false],
 	['5/4\n', false],
+
+	// sub-beats durations
+	['F C (A (B', false],
+	['F C A B)', false],
+	['F (C A) (B', false],
+	['F (C A(add b9)) B)', false],
 ])('Test Chord line %s', (line, output) => {
 	test('Correctly detect chord line', () => {
 		expect(isChordLine(line)).toEqual(output);
