5f1c2d2a7edfb8048a02878525cc37c8655f1442
Christophe Noël <touffi51@gmail.com>
2023-01-04 18:52:47 +0100

    Render time signature on change

--- packages/chord-mark/src/renderer/components/renderChordLine.js
+++ packages/chord-mark/src/renderer/components/renderChordLine.js
@@ -1,28 +1,43 @@
+import _cloneDeep from 'lodash/cloneDeep';
+import _isEqual from 'lodash/isEqual';
+
 import chordLineTpl from './tpl/chordLine.js';
 
 import renderBarContent from './renderBarContent';
 import barSeparatorTpl from './tpl/barSeparator.js';
+
+import { defaultTimeSignature } from '../../parser/syntax';
 import symbols from '../symbols';
 
 /**
  * @param {ChordLine} chordLineModel
+ * @param {TimeSignature} contextTimeSignature
  * @param {Boolean} shouldPrintBarSeparators
  * @param {Boolean} shouldPrintSubBeatDelimiters
  * @returns {String} rendered html
  */
 export default function renderChordLine(
 	chordLineModel,
-	shouldPrintBarSeparators,
-	shouldPrintSubBeatDelimiters = true
+	contextTimeSignature = defaultTimeSignature,
+	{
+		shouldPrintBarSeparators = true,
+		shouldPrintSubBeatDelimiters = true,
+	} = {}
 ) {
+	let previousTimeSignature = _cloneDeep(contextTimeSignature);
 	const allBarsRendered = chordLineModel.allBars.map((bar, i) => {
 		const isLastBar = !chordLineModel.allBars[i + 1];
-		return renderBarContent(
-			bar,
-			isLastBar,
-			shouldPrintBarSeparators,
-			shouldPrintSubBeatDelimiters
+		const shouldPrintTimeSignature = !_isEqual(
+			bar.timeSignature,
+			previousTimeSignature
 		);
+		const barContent = renderBarContent(bar, isLastBar, {
+			shouldPrintBarSeparators,
+			shouldPrintSubBeatDelimiters,
+			shouldPrintTimeSignature,
+		});
+		previousTimeSignature = _cloneDeep(bar.timeSignature);
+		return barContent;
 	});
 
 	const barSeparator = shouldPrintBarSeparators
